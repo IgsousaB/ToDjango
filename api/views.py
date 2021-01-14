8108060f5d6ffb90b38d938ac1f952a134cd6359
Igor Baldoino <igortthq@gmail.com>
2021-01-14 12:37:34 -0300

    Finished API with CRUD Functionality

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from .serializers import TaskSerializer
 
 from .models import Task
 
+
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
@@ -18,11 +19,13 @@ def apiOverview(request):
     }
     return Response(api_urls)
 
+
 @api_view(['GET'])
 def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def taskDetail(request, pk):
@@ -31,8 +34,31 @@ def taskDetail(request, pk):
     return Response(serializer.data)
 
 
+@api_view(['POST'])
+def taskCreate(request):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=True)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
 
 
-    # return Response(api_urls)
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=True)
 
-# Create your views here.
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return Response("Item successfully deleted!")
